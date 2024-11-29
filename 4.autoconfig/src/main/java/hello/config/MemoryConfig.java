a8b42494df5abef27fd0356f1e76cba2edf2ac6f
nickhealthy <alskadmlcraz1@gmail.com>
2024-11-30 01:00:01 +0900

    5. 자동 구성 - @Conditional

--- 4.autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ 4.autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,14 +1,18 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 메모리 관련 라이브러리 설정
+ * @Conditional(MemoryCondition.class) - matches() 가 true 일때만 해당 빈 적용
  */
 @Configuration
+@Conditional(MemoryCondition.class)
 public class MemoryConfig {
 
     @Bean
